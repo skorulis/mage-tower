9d571d89f4eccb782afdf5ab99d86a68aeff19df
Alex Skorulis <skorulis@gmail.com>
2025-09-10 18:15:47 +1000

    Initial commit

---
+++ MageTower/ContentView.swift
@@ -0,0 +1,19 @@
+//  Created by Alexander Skorulis on 10/9/2025.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MageTower/MageTowerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MageTowerApp.swift
+//  MageTower
+//
+//  Created by Alexander Skorulis on 10/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct MageTowerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
