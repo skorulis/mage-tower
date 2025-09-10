5f7cb2de13c475187dfa8fbd1d766f4d5e239354
Alex Skorulis <skorulis@gmail.com>
2025-09-10 20:04:16 +1000

    Start spawning squares

--- MageTower/GameScene.swift
+++ MageTower/GameScene.swift
@@ -16,7 +16,9 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private let spawnService = SpawnService()
     
     private var circleNode: SKShapeNode?
-    private var squareNode: SKShapeNode?
+    
+    private var squares: [UUID: SKNode] = [:]
+    
     override init(size: CGSize) {
         super.init(size: size)
         scaleMode = .resizeFill
@@ -50,14 +52,19 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
 
     override func update(_ currentTime: TimeInterval) {
-        guard let circle = circleNode, let square = squareNode, let body = square.physicsBody else { return }
-        let toCircle = CGVector(dx: circle.position.x - square.position.x,
-                                dy: circle.position.y - square.position.y)
-        let distance = max(1.0, sqrt(toCircle.dx * toCircle.dx + toCircle.dy * toCircle.dy))
-        let normalized = CGVector(dx: toCircle.dx / distance, dy: toCircle.dy / distance)
-        let forceMagnitude: CGFloat = 50.0
-        let force = CGVector(dx: normalized.dx * forceMagnitude, dy: normalized.dy * forceMagnitude)
-        body.applyForce(force)
+        guard let circle = circleNode else { return }
+        for square in squares.values {
+            guard let body = square.physicsBody else {
+                continue
+            }
+            let toCircle = CGVector(dx: circle.position.x - square.position.x,
+                                    dy: circle.position.y - square.position.y)
+            let distance = max(1.0, sqrt(toCircle.dx * toCircle.dx + toCircle.dy * toCircle.dy))
+            let normalized = CGVector(dx: toCircle.dx / distance, dy: toCircle.dy / distance)
+            let forceMagnitude: CGFloat = 50.0
+            let force = CGVector(dx: normalized.dx * forceMagnitude, dy: normalized.dy * forceMagnitude)
+            body.applyForce(force)
+        }
     }
     
     func addEnemy() {
@@ -68,6 +75,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         squareNode.fillColor = .white
         squareNode.strokeColor = .clear
         squareNode.position = enemy.spawnPosition
+        squareNode.userData = ["id": enemy.id]
         squareNode.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: squareSize, height: squareSize))
         squareNode.physicsBody?.allowsRotation = false
         squareNode.physicsBody?.linearDamping = 2.0
@@ -75,16 +83,22 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         squareNode.physicsBody?.categoryBitMask = PhysicsCategory.square
         squareNode.physicsBody?.contactTestBitMask = PhysicsCategory.circle
         addChild(squareNode)
-        self.squareNode = squareNode
         
+        self.squares[enemy.id] = squareNode
     }
 
     func didBegin(_ contact: SKPhysicsContact) {
         let categories = contact.bodyA.categoryBitMask | contact.bodyB.categoryBitMask
         if categories == (PhysicsCategory.circle | PhysicsCategory.square) {
-            print("Square hit circle")
-            self.squareNode?.removeFromParent()
-            self.squareNode = nil
+            let squareBody = contact.bodyA.categoryBitMask == PhysicsCategory.square ? contact.bodyA : contact.bodyB
+            let hitNode = squareBody.node as? SKShapeNode
+            guard let uuid = squareBody.node?.userData?["id"] as? UUID else {
+                return
+            }
+            print("Square hit circle: id=\(uuid)")
+            
+            squares.removeValue(forKey: uuid)
+            hitNode?.removeFromParent()
             
             addEnemy()
         }
---
+++ MageTower/Service/GameService.swift
@@ -0,0 +1,7 @@
+//  Created by Alexander Skorulis on 10/9/2025.
+
+import Foundation
+
+final class GameService {
+    
+}
